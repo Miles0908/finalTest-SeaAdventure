5a43a3c0c7babc790f18dedc7d5671dc82376522
Claudio Ventimiglia <claudioxxmilia@yahoo.it>
2023-08-03 15:24:18 +0200

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import styles from "./App.module.scss"
+
+function App() {
+
+
+  return (
+    <>
+      <div className={styles.App}>ciao sono il div di app</div>
+    </>
+  )
+}
+
+export default App
